97ab6d8dcec5c0acd09fc76aa224238d9439abd9
samletwin <samletwin@yahoo.ca>
2024-07-16 01:44:52 -0400

    edit adc module to use mcp3202

--- src/main/adc.cpp
+++ src/main/adc.cpp
@@ -3,20 +3,28 @@
 #include "esp32-hal-adc.h"
 #include "custom_types.h"
 #include "adc.h"
+#include "mcp320x.h"
 #include "webpage/webpage.h"
 
 /* ------------------------------------------------------------------------------------------------
   DEFINES
 ------------------------------------------------------------------------------------------------ */
-#define ADC_BATT_CURRENT_IN   ADC1_CHANNEL_2    // ADC1_CHANNEL_0
-#define ADC_BATT_VOLTAGE_IN   ADC1_CHANNEL_3    // ADC1_CHANNEL_1
+#define ADC_BATT_CURRENT_IN   MCP3202::Channel::SINGLE_0    
+#define ADC_BATT_VOLTAGE_IN   MCP3202::Channel::SINGLE_1
 
-#define R1 9970
-#define R2 19890
+#define R1 10000
+#define R2 20000
 #define ADC_BATT_VOLTAGE_SCALE (R1+R2)/R2
 #define ADC_CURRENT_SCALE (1000/400) /* 400mV/A scaling - divide by 400 to get A, multiply by 1000 to get mA*/
 
-#define ADC_DEFAULT_SAMPLE_RATE_HZ 100
+#define ADC_DEFAULT_SAMPLE_RATE_HZ 10000
+
+#define SPI_CS    	13 		   // SPI slave select
+#define SPI_CLK     19
+#define SPI_MOSI    22
+#define SPI_MISO    21
+#define ADC_VREF    5000     // 5V Vref
+#define ADC_CLK     1600000  // SPI clock 1.6MHz
 /* ------------------------------------------------------------------------------------------------
   GLOBALS
 ------------------------------------------------------------------------------------------------ */
@@ -29,8 +37,7 @@ extern webpageGlobalData_s globalWebpageData_s;
 /* ------------------------------------------------------------------------------------------------
   LOCAL VARIABLES
 ------------------------------------------------------------------------------------------------ */
-uint64_t runningTimer_ms_ui64 = 0;
-uint64_t currentTimer_ms_ui64 = 0;
+static MCP3202 adc(ADC_VREF, SPI_CS);
 
 /* ------------------------------------------------------------------------------------------------
   FUNCTION PROTOTYPES
@@ -44,44 +51,41 @@ void setupTimer(uint8_t timerNumber, uint32_t intervalMicros, void (*callback)()
     timerAlarmEnable(timerAdc); // Enable the timer
 }
 
-// Callback function that gets called by the hardware timer interrupt
 void IRAM_ATTR onTimer() {
-    // Your code here, e.g., read ADC, toggle a pin, etc.
-    adc_task();
-    Serial.println("Timer Interrupt Triggered");
+  adc_task();
 }
 
 void adc_setup() {
-  /* Set pin modes */
-  pinMode(ADC_BATT_CURRENT_IN, INPUT);
-  pinMode(ADC_BATT_VOLTAGE_IN, INPUT);
 
   setupTimer(1, ADC_DEFAULT_SAMPLE_RATE_HZ * 1000, adc_task);
 
-  /* Setup ADC */
-  analogSetAttenuation(ADC_11db); 
-  adc1_config_width(ADC_WIDTH_BIT_12);
+  /* Setup SPI for MCP3202 */
+  pinMode(SPI_CS, OUTPUT);
+  digitalWrite(SPI_CS, HIGH);
+
+  SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
+  SPI.begin(SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS);
+  SPI.beginTransaction(settings);
 }
 
 float32 adc_readCurrent() {
-  return analogReadMilliVolts(ADC_BATT_CURRENT_IN) * ADC_CURRENT_SCALE;
+  uint16_t digital = adc.read(ADC_BATT_CURRENT_IN);
+  uint16_t raw = adc.toAnalog(digital);
+  float32 current_mA = static_cast<float32>((raw - 2500) *ADC_CURRENT_SCALE);
+  return current_mA;
 }
 
 float32 adc_readBatteryVoltage() {
-  return analogReadMilliVolts(ADC_BATT_VOLTAGE_IN) * ADC_BATT_VOLTAGE_SCALE;
+  uint16_t digital = adc.read(ADC_BATT_VOLTAGE_IN);
+  uint16_t raw = adc.toAnalog(digital);
+  float32 voltage_mV = static_cast<float32>(raw *ADC_BATT_VOLTAGE_SCALE);
+  return voltage_mV;
 }
 
 
 void IRAM_ATTR adc_task() {
-  runningTimer_ms_ui64 = millis();
-  uint32 volt_rawAdc_ui32;
-  sint32 current_rawAdc_ui32;
-  volt_rawAdc_ui32 = analogReadMilliVolts(ADC_BATT_VOLTAGE_IN);
-  current_rawAdc_ui32 = analogReadMilliVolts(ADC_BATT_CURRENT_IN) - 2500;
-
-
-  globalWebpageData_s.voltageReading_mv_f32 = ((double)volt_rawAdc_ui32) * ADC_BATT_VOLTAGE_SCALE; 
-  globalWebpageData_s.currentReading_mA_f32 = ((double)current_rawAdc_ui32) * 2.5; /* TI Sensor has 400mV/A scaling */
+  globalWebpageData_s.voltageReading_mv_f32 = adc_readBatteryVoltage();
+  globalWebpageData_s.currentReading_mA_f32 = adc_readCurrent();
 
 
   if (globalWebpageData_s.sampleRateChanged_b == true && globalWebpageData_s.sampleRate_Hz_ui16 > 0) {
@@ -91,26 +95,9 @@ void IRAM_ATTR adc_task() {
   }
   
   #ifdef DEBUG_ADC_LOG
-  Serial.print("Battery_Voltage:");
   Serial.print(globalWebpageData_s.voltageReading_mv_f32);
-  Serial.print(",Battery_Current:");
+  Serial.print(",");
   Serial.println(globalWebpageData_s.currentReading_mA_f32);
-  #ifdef DEBUG_ADC
-  // if (runningTimer_ms_ui64 >= currentTimer_ms_ui64 + 2000)
-  // {
-    currentTimer_ms_ui64 = runningTimer_ms_ui64;
-    Serial.print("Current voltage scaled reading: ");
-    Serial.println(globalWebpageData_s.voltageReading_mv_f32);
-    Serial.print("Raw voltage divider reading: ");
-    Serial.println(volt_rawAdc_ui32);
-
-    Serial.print("Current current scaled reading: ");
-    Serial.println(globalWebpageData_s.currentReading_mA_f32);
-    Serial.print("Raw current reading: ");
-    Serial.println(current_rawAdc_ui32);
-    Serial.print("\n");
-  // }
-  #endif
   #endif
 }
 
